90439383a000747ee5dab0a6102e386e4d896354
Asmaa Ayyash <asmaaayyash22@gmail.com>
2018-12-20 03:58:25 +0300

    initial edit

--- private/models/propriety.class.php
+++ private/models/propriety.class.php
@@ -1,10 +1,9 @@
 <?php
 
-class Admin {
+class Propriety {
   static protected $database;
 
   static public function set_database($database) {
     self::$database = $database;
   }
-
 }
--- private/models/service.class.php
+++ private/models/service.class.php
@@ -1,10 +1,10 @@
 <?php
 
-class Admin {
+class Service {
   static protected $database;
 
   static public function set_database($database) {
     self::$database = $database;
   }
 
-}
+ ?>
